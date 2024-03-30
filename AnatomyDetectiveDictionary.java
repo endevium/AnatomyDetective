7cdc95dd9158b19e4cf38e33b0f69c2de0ff78ad
endevium <148771728+endevium@users.noreply.github.com>
2024-03-30 20:47:07 +0800

    Add files via upload

---
+++ AnatomyDetectiveDictionary.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class AnatomyDetectiveDictionary {
+	private Map<String, String> dictionary;
+	
+	public AnatomyDetectiveDictionary() {
+		dictionary = new HashMap<>();
+		dictionary.put("skull", ".//assets/skull.png");
+		dictionary.put("leg", ".//assets/leg.png");
+		dictionary.put("hand", ".//assets/hand.png");
+		dictionary.put("eye", ".//assets/eye.png");
+		dictionary.put("liver", ".//assets/liver.png");
+		dictionary.put("kidney", ".//assets/kidney.png");
+		dictionary.put("lung", ".//assets/lung.png");
+		dictionary.put("teeth", ".//assets/teeth.png");
+		dictionary.put("neck", ".//assets/neck.png");
+		dictionary.put("stomach", ".//assets/stomach.png");
+		dictionary.put("brain", ".//assets/brain.png");
+	}
+	
+	public Map<String, String> accessDictionary() {
+		return dictionary;
+	}
+}
+
