34dd00c77ed46c2cbe38cef955b29a2c8cefeb70
hxt-iiixi <147921176+hxt-iiixi@users.noreply.github.com>
2024-03-31 01:40:53 +0800

    WITH TIMER

---
+++ src/game.java
@@ -0,0 +1,366 @@
+package com.BIT5.AnatomyDetective;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+import java.awt.event.*;
+import java.security.cert.Extension;
+
+public class game {
+
+    private static final JProgressBar PBar = new JProgressBar();
+    private static JFrame main_frame;
+    private static JLabel gameLogoLabel, scoreLabel, roundLabel, imageLabel, totalLabel;
+    private static JTextField answerInputField;
+    private static JButton playBtn, submitBtn, skipBtn, tryAgainBtn, menuBtn, backBtn;
+    private static String correctAnswer; 
+    private static JPanel main_panel, game_panel, game_over_panel, back_panel;
+    private static int score;
+    private static int roundNum;
+
+    
+    
+    /* 
+     IMPORTANT NOTES:
+     Map - dictionary in Java (java.util.Map), used as a dictionary for the BODY PARTS
+     Set - dictionary where EVERY item is unique, used here to check if the body is ALREADY USED
+     HashSet<>() - creates a new empty set
+     
+     JFrame - the GUI window
+     Container - used to organize panels, frames, dialog, window, etc
+     JPanel - used as a container for labels, buttons, text field, etc
+     JTextField - SINGLE LINE text field
+     JTextArea - not used here, MULTIPLE LINES text field
+     .getContentPane() - used to retrieve the layer that holds the objects of the frame
+     (Review functions)
+     
+     Dimension - SETS the WIDTH and HEIGHT of the object (java.awt.Dimension)
+     ex: (new Dimension(WIDTH, HEIGHT));
+     ImageIcon - used for images (javax.swing.ImageIcon)
+     GridBagLayout - aligns components VERTICALLY
+     GridBagConstraints - the constraints for the layout (x and y)
+     addActionListener and ActionListener - only used for BUTTONS to add actions after clicking the button
+     - requires public void actionPerformed(actionEvent e) after
+    */
+
+    public static void main(String[] args) {
+        main_frame = new JFrame("Anatomy Detective v0.0.1");
+        main_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        main_frame.setSize(1024, 950);
+        main_frame.setResizable(false);
+        main_frame.setLocationRelativeTo(null);
+        main_frame.getContentPane().setBackground(Color.LIGHT_GRAY);
+
+        Container container = main_frame.getContentPane();
+        container.setLayout(null);
+        
+        score = 0;
+        roundNum = 1;
+        
+        main_panel = new JPanel(new GridBagLayout());
+        main_panel.setBackground(Color.LIGHT_GRAY);
+        main_panel.setBounds(250, 0, 500, 700);
+        container.add(main_panel);
+        
+        gameLogoLabel = new JLabel();
+        gameLogoLabel.setIcon(new ImageIcon("com/BIT5/AnatomyDetective/assets/anatomy_detective_logo.png"));
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.insets = new Insets(80, 0, 20, 0);
+        main_panel.add(gameLogoLabel, gbc);
+        
+        playBtn = new JButton();
+        playBtn.setIcon(new ImageIcon("com/BIT5/AnatomyDetective/assets/play_button.png"));
+        playBtn.setBackground(null);
+        playBtn.setBorder(null);
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        playBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {	
+            	score = 0;
+            	roundNum = 1;
+            	scoreLabel.setText(Integer.toString(score));
+            	totalLabel.setText("Score: " + Integer.toString(score));
+        		roundLabel.setText(Integer.toString(roundNum) + "/10");
+        		usedParts.clear();
+            	generatePart();
+            	
+            	main_panel.setVisible(false);
+            	game_panel.setVisible(true);
+            	back_panel.setVisible(true);
+                
+                fill();
+
+            }
+        });
+        main_panel.add(playBtn, gbc);
+        
+        back_panel = new JPanel();
+        back_panel.setBackground(Color.LIGHT_GRAY);
+        back_panel.setBounds(14, 10, 30, 30);
+        back_panel.setVisible(false);
+        container.add(back_panel);
+        
+        backBtn = new JButton();
+        backBtn.setIcon(new ImageIcon("com/BIT5/AnatomyDetective/assets/back_button.png"));
+        backBtn.setBackground(null);
+        backBtn.setBorder(null);
+        backBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	game_panel.setVisible(false);
+            	back_panel.setVisible(false);
+            	main_panel.setVisible(true);
+            	usedParts.clear();
+            }
+        });
+        back_panel.add(backBtn);
+        
+        game_panel = new JPanel(new GridBagLayout());
+        game_panel.setBackground(Color.LIGHT_GRAY);
+        game_panel.setBounds(250, 60, 500, 700);
+        game_panel.setVisible(false);
+        container.add(game_panel);
+
+        
+
+        scoreLabel = new JLabel(Integer.toString(score));
+        scoreLabel.setFont(new Font("Arial", Font.BOLD, 40));
+        scoreLabel.setForeground(Color.BLACK);
+        GridBagConstraints gbc2 = new GridBagConstraints();
+        gbc2.gridx = 0;
+        gbc2.gridy = 0;
+        gbc2.insets = new Insets(10, 0, 10, 0);
+        game_panel.add(scoreLabel, gbc2);
+        
+        PBar.setValue(0);
+        PBar.setBounds(0,0,420,100);
+        PBar.setForeground(Color.black);
+        PBar.setBackground(Color.white);
+        gbc2.gridx = 0;
+        gbc2.gridy = 0;
+        game_panel.add(PBar, gbc2);
+        
+        
+
+        roundLabel = new JLabel(Integer.toString(roundNum) + "/10");
+        roundLabel.setFont(new Font("Arial", Font.PLAIN, 21));
+        roundLabel.setForeground(Color.BLACK);
+        gbc2.gridx = 0;
+        gbc2.gridy = 2;
+        game_panel.add(roundLabel, gbc2);
+
+        generatePart();
+
+        answerInputField = new JTextField("");
+        answerInputField.setFont(new Font("Helvetica Neue", Font.BOLD, 19));
+        answerInputField.setPreferredSize(new Dimension(400, 40));
+        answerInputField.setHorizontalAlignment(JTextField.CENTER);
+        gbc2.gridx = 0;
+        gbc2.gridy = 4;
+        game_panel.add(answerInputField, gbc2);
+
+
+
+        submitBtn = new JButton();
+        submitBtn.setIcon(new ImageIcon("com/BIT5/AnatomyDetective/assets/submit_btn.png"));
+        submitBtn.setBackground(null);
+        submitBtn.setBorder(null);
+        submitBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String userAnswer = answerInputField.getText();
+                if (userAnswer.equalsIgnoreCase(correctAnswer) && roundNum == 10) {
+                	score += 100;
+                	totalLabel.setText("Score: " + Integer.toString(score));
+                	back_panel.setVisible(false);
+                	game_panel.setVisible(false);
+                	game_over_panel.setVisible(true);
+                    answerInputField.setText("");
+                    generatePart();
+                }
+                else if (userAnswer.equalsIgnoreCase(correctAnswer) && roundNum < 11) {
+                    score += 100;
+                    roundNum += 1;
+                    scoreLabel.setText(Integer.toString(score));
+                    totalLabel.setText("Score: " + Integer.toString(score));
+                    roundLabel.setText(Integer.toString(roundNum) + "/10");
+                    answerInputField.setText("");
+                    generatePart();
+                } 
+                else {
+                	answerInputField.setText("");
+                }               
+            }
+        });
+        gbc2.gridx = 0;
+        gbc2.gridy = 5;
+        game_panel.add(submitBtn, gbc2);
+
+       
+
+        skipBtn = new JButton();
+        skipBtn.setIcon(new ImageIcon("com/BIT5/AnatomyDetective/assets/skip_button.png"));
+        skipBtn.setBackground(null);
+        skipBtn.setBorder(null);
+        skipBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	if (roundNum < 10) {
+            		roundNum += 1;
+            		scoreLabel.setText(Integer.toString(score));
+            		totalLabel.setText("Score: " + Integer.toString(score));
+            		roundLabel.setText(Integer.toString(roundNum) + "/10");
+            		answerInputField.setText("");
+            		generatePart();
+            	} 
+            	else if (roundNum == 10) {
+            		scoreLabel.setText(Integer.toString(score));
+            		totalLabel.setText("Score: " + Integer.toString(score));
+            		roundLabel.setText(Integer.toString(roundNum) + "/10");
+            		answerInputField.setText("");
+            		back_panel.setVisible(false);
+                	game_panel.setVisible(false);
+                	game_over_panel.setVisible(true);
+                }
+            }
+        });
+        gbc2.gridx = 0;
+        gbc2.gridy = 6;
+        game_panel.add(skipBtn, gbc2);
+        
+        game_over_panel = new JPanel(new GridBagLayout());
+        game_over_panel.setBackground(Color.LIGHT_GRAY);
+        game_over_panel.setBounds(250, 0, 500, 700);
+        game_over_panel.setVisible(false);
+        container.add(game_over_panel);
+        
+        totalLabel = new JLabel("Score: " + score);
+        totalLabel.setFont(new Font("Arial", Font.BOLD, 40));
+        totalLabel.setForeground(Color.BLACK);
+        GridBagConstraints gbc3 = new GridBagConstraints();
+        gbc3.gridx = 0;
+        gbc3.gridy = 0;
+        gbc3.insets = new Insets(10, 0, 10, 0);
+        game_over_panel.add(totalLabel, gbc3);
+        
+        tryAgainBtn = new JButton();
+        tryAgainBtn.setIcon(new ImageIcon("com/BIT5/AnatomyDetective/assets/try_again_button.png"));
+        tryAgainBtn.setBackground(null);
+        tryAgainBtn.setBorder(null);
+        tryAgainBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) { 
+            	back_panel.setVisible(true);
+            	game_over_panel.setVisible(false);
+            	score = 0;
+            	roundNum = 1;
+            	scoreLabel.setText(Integer.toString(score));
+            	totalLabel.setText("Score: " + Integer.toString(score));
+        		roundLabel.setText(Integer.toString(roundNum) + "/10");
+        		usedParts.clear();
+            	generatePart();
+            	back_panel.setVisible(true);
+            	game_panel.setVisible(true);
+            }
+        });
+        gbc3.gridx = 0;
+        gbc3.gridy = 2;
+        game_over_panel.add(tryAgainBtn, gbc3);
+        
+        menuBtn = new JButton();
+        menuBtn.setIcon(new ImageIcon("com/BIT5/AnatomyDetective/assets/menu_button.png"));
+        menuBtn.setBackground(null);
+        menuBtn.setBorder(null);
+        menuBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) { 
+            	back_panel.setVisible(false);
+            	game_over_panel.setVisible(false);
+            	score = 0;
+            	roundNum = 1;
+            	scoreLabel.setText(Integer.toString(score));
+        		totalLabel.setText("Score: " + Integer.toString(score));
+        		roundLabel.setText(Integer.toString(roundNum) + "/10");
+        		usedParts.clear();
+            	generatePart();
+            	main_panel.setVisible(true);
+            }
+        });
+        gbc3.gridx = 0;
+        gbc3.gridy = 3;
+        game_over_panel.add(menuBtn, gbc3);
+        
+        main_frame.setVisible(true);
+        
+    }
+
+
+
+    static Set<String> usedParts = new HashSet<>();
+    // Every item is unique in a HashSet
+
+    static void generatePart() {
+        AnatomyDetectiveDictionary con = new AnatomyDetectiveDictionary();
+        Map<String, String> dictionary = con.accessDictionary();
+        
+        if (usedParts.size() == dictionary.size()) {
+            usedParts.clear();
+        }
+
+        Random random = new Random();
+        String partName = null;
+        
+        // While loop to check if the random part is already chosen
+        do {
+            partName = (String) dictionary.keySet().toArray()[random.nextInt(dictionary.size())];
+        } while (usedParts.contains(partName));
+
+        usedParts.add(partName);
+
+        correctAnswer = partName;
+        String imageDir = dictionary.get(partName);
+
+        ImageIcon partImage = createResizedImageIcon(imageDir, 500, 400);
+
+        if (imageLabel != null) {
+            game_panel.remove(imageLabel);
+        }
+
+        imageLabel = new JLabel(partImage);
+        GridBagConstraints gbcImage = new GridBagConstraints();
+        gbcImage.gridx = 0;
+        gbcImage.gridy = 3;
+        game_panel.add(imageLabel, gbcImage);
+        game_panel.revalidate();
+        game_panel.repaint();
+    }
+
+    static ImageIcon createResizedImageIcon(String path, int width, int height) {
+        ImageIcon icon = new ImageIcon(path);
+        Image image = icon.getImage();
+        Image scaledImage = image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        return new ImageIcon(scaledImage);
+    }
+    static void fill() {
+        Thread timer = new Thread(new Runnable() {
+            public void run() {
+                int count = 0;
+                while (count <= 100) {
+                    PBar.setValue(count);
+                    try {
+                        Thread.sleep(590);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    count ++;
+                }
+            }
+        });
+        timer.start();
+    }
+}
+
